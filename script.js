7775a705a711861ffd98e7183b5c72c4f1d8ad96
Ron Zapanta <ron.zapanta1221@gmail.com>
2023-12-20 12:15:42 -0500

    Created calculator app and Added addition function

---
+++ script.js
@@ -0,0 +1,43 @@
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+
+function appendToDisplay(value) {
+    if (!operator) {
+        operand1 += value;
+    } else {
+        operand2 += value;
+    }
+    //if value is + then setOperator(value)
+    if (value == '+'){
+        setOperator(value);
+    } else {
+        document.getElementById('display').value += value;
+    }
+    
+}
+function setOperator(op) {
+    if (operand1) {
+        operator = op;
+        document.getElementById('display').value += " " + op + " ";
+    }
+}
+function calculate() {
+    let result;
+    switch (operator) {
+        case '+':
+            result = parseFloat(operand1) + parseFloat(operand2);
+            break;
+    }
+    document.getElementById('display').value = result;
+    operand1 = result.toString();
+    operand2 = "";
+    operator = "";
+}
+
+function clearDisplay() {
+    operand1 = "";
+    operand2 = "";
+    operator = "";
+    document.getElementById('display').value = "";
+}
